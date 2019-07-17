16a5592d6e7024f043d04292771775b84daaad4d
devinzhang0209 <devin.w.zhang@qq.com>
2019-07-17 21:18:01 +0800

    二维码

---
+++ src/main/java/com/utools/backend/controller/Base64Controller.java
@@ -0,0 +1,11 @@
+package com.utools.backend.controller;
+
+/**
+ * @author Devin Zhang
+ * @className Base64Controller
+ * @description TODO
+ * @date 2019/7/17 20:50
+ */
+
+public class Base64Controller {
+}
---
+++ src/main/java/com/utools/backend/controller/QrCodeController.java
@@ -0,0 +1,11 @@
+package com.utools.backend.controller;
+
+/**
+ * @author Devin Zhang
+ * @className QrCodeController
+ * @description TODO
+ * @date 2019/7/17 20:50
+ */
+
+public class QrCodeController {
+}
