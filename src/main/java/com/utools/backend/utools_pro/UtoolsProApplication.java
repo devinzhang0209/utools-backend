b9f07e685fc0d4a47bf64eca8ce06c6f208b7bb8
devinzhang1990 <devin.w.zhang@qq.com>
2019-07-17 18:11:29 +0800

    初始导入

---
+++ src/main/java/com/utools/backend/utools_pro/UtoolsProApplication.java
@@ -0,0 +1,13 @@
+package com.utools.backend.utools_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtoolsProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtoolsProApplication.class, args);
+    }
+
+}
